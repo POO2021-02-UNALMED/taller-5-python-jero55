33fd2a27221cdaed39a9b8df49ee42bdc9187502
jero55 <jsalazars@unal.edu.co>
2021-11-23 23:18:07 -0500

    Create zoologico.py

---
+++ gestion/zoologico.py
@@ -0,0 +1,36 @@
+from gestion.zona import Zona
+class Zoologico:
+
+    def __init__(self,nombre,ubicacion):
+        self._nombre= nombre
+        self._ubicacion= ubicacion
+        self._zonas=[]
+
+    def getNombre(self):
+        return self._nombre
+    
+    def setNombre(self,nombre):
+        self._nombre = nombre
+
+    def getUbicacion(self):
+        return self._ubicacion
+
+    def setUbicacion(self,ubicacion):
+        self._ubicacion= ubicacion
+    
+    def getZona(self):
+        return self._zonas
+
+    def setZona(self,zona):
+        self._zonas = zona
+
+    
+    def agregarZonas(self,zona):
+        self._zonas.append(zona)
+
+    def cantidadTotalAnimales(self):
+        c = 0
+        for i in  self._zonas:
+            c += i.cantidadAnimales()
+        
+        return c
